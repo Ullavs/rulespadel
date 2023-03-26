6946c9bf203c79a482b53e5b2f9ef97012156614
Ullavs <ullavansprang@gmail.com>
2023-03-26 22:24:48 +0200

    start with adding language switch

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,51 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../components/Card";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import copy from "../copy";
+import enCopy from "../copy/en.json";
+import nlCopy from "../copy/nl.json";
 
 export function Home() {
+  const [language, setLanguage] = useState("en");
+
+  const translations = language === "en" ? enCopy : nlCopy;
+
+  const switchLanguage = () => {
+    setLanguage((prevLanguage) => {
+      return prevLanguage === "en" ? "nl" : "en";
+    });
+  };
+
   return (
     <>
       <main>
-        <Header title={copy.home.title} subtitle={copy.home.subtitle} />
+        <Header
+          title={translations.home.title}
+          subtitle={translations.home.subtitle}
+        />
+        <button className="language-switcher" onClick={switchLanguage}>
+          {language === "en" ? "Wijzig naar Nederlands" : "Switch to English"}
+        </button>
         <div>
           <p className="box">
-            <strong>{copy.home.introductionStrong}</strong>
-            {copy.home.introduction}
+            <strong>{translations.home.introductionStrong}</strong>
+            {translations.home.introduction}
             <br />
-            {copy.home.introductionEnd}
+            {translations.home.introductionEnd}
           </p>
           <ul role="list" className="card-grid">
             <Card
               href="/spelregels"
-              title={copy.home.rulesTitle}
-              body={copy.home.rulesCardBody}
+              title={translations.home.rulesTitle}
+              body={translations.home.rulesCardBody}
             />
             <Card
               href="/materiaal"
-              title={copy.home.gearTitle}
-              body={copy.home.gearCardBody}
+              title={translations.home.gearTitle}
+              body={translations.home.gearCardBody}
             />
             <Card
               href="/speelveld"
-              title={copy.home.playingFieldTitle}
-              body={copy.home.playingFieldCardBody}
+              title={translations.home.playingFieldTitle}
+              body={translations.home.playingFieldCardBody}
             />
             <Card
               href="/techniekentactiek"
-              title={copy.home.technicAndTacticsTitle}
-              body={copy.home.technicAndTacticsCardBody}
+              title={translations.home.technicAndTacticsTitle}
+              body={translations.home.technicAndTacticsCardBody}
             />
             <Card
               href="/padelinnederland"
-              title={copy.home.padelInNLTitle}
-              body={copy.home.padelInNLCardBody}
+              title={translations.home.padelInNLTitle}
+              body={translations.home.padelInNLCardBody}
             />
             <Card
               href="/worldpadeltour"
-              title={copy.home.worldPadelTourTitle}
-              body={copy.home.worldPadelTourCardBody}
+              title={translations.home.worldPadelTourTitle}
+              body={translations.home.worldPadelTourCardBody}
             />
           </ul>
         </div>
